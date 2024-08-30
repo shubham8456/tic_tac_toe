2340b0838f637c5f24f811cca268c05110cfe962
Shubham Rawat <shubham.rawat@plansource.com>
2024-08-30 20:18:35 +0530

    Lecture 73 | add basic html structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-
 function App() {
   return (
-    <></>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+
+        GAME BOARD
+      </div>
+
+      LOG
+    </main>
   )
 }
 
